b2b2b1ecc8ddad5def14e175d3229faeeeb91d78
Kanye East <79680478+TheRealKanyeEast@users.noreply.github.com>
2021-08-19 13:19:43 +0530

    Update Finity.lua

--- Finity.lua
+++ Finity.lua
@@ -61,17 +61,17 @@ finity.dark_theme = { -- dark
 	main_container = Color3.fromRGB(32, 32, 33),
 	separator_color = Color3.fromRGB(63, 63, 65),
 
-	text_color = Color3.fromRGB(206, 206, 206),
+	text_color = Color3.fromRGB(64, 247, 156),
 
 	category_button_background = Color3.fromRGB(63, 62, 65),
 	category_button_border = Color3.fromRGB(72, 71, 74),
 
-	checkbox_checked = Color3.fromRGB(222, 53, 149),
+	checkbox_checked = Color3.fromRGB(64, 247, 156),
 	checkbox_outer = Color3.fromRGB(84, 81, 86),
 	checkbox_inner = Color3.fromRGB(132, 132, 136),
 
-	slider_color = Color3.fromRGB(222, 53, 149),
-	slider_color_sliding = Color3.fromRGB(222, 53, 149),
+	slider_color = Color3.fromRGB(64, 247, 156),
+	slider_color_sliding = Color3.fromRGB(64, 247, 156),
 	slider_background = Color3.fromRGB(88, 84, 90),
 	slider_text = Color3.fromRGB(177, 177, 177),
 
